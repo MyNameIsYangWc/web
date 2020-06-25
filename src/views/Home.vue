2204fca46ec2ace959451adf72300d60bea60dce
MyNameIsYwc <1414702504@qq.com>
2020-06-25 21:30:47 +0800

    初始化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,6 @@
 <template>
     <div class="page">
         <h2>Home Page</h2>
-        <h3>{{$t('common.doc')}}</h3>
-        <el-button type="success" @click="changeLanguage('zh_cn')">简体中文</el-button>
-        <el-button type="success" @click="changeLanguage('en_us')">English</el-button>
         <li class="fa fa-home fa-fw"></li>
         <el-button type="primary" @click="testAxios()">测试Axios调用</el-button>
         <el-button type="primary" @click="getUser()">獲取用戶信息</el-button>
@@ -29,11 +26,6 @@ export default {
             axios.get('http://localhost:8080/menu').then(res=>{
                 alert(JSON.stringify(res.data))
             })
-        },
-        changeLanguage(lang){
-          lang==='' ? 'zh_cn':lang
-          this.$il8n.locale=lang
-          this.langVisible=false
         }
     }
 }
