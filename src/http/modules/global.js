a901f80c5bbda8276ff5dab2c2522c3731b9466c
MyNameIsYwc <1414702504@qq.com>
2020-06-28 21:54:29 +0800

    主页

--- src/http/modules/global.js
+++ src/http/modules/global.js
@@ -4,9 +4,9 @@
  * 通过this.global调用
  */
 //后台管理系统服务器地址
-export const baseUrl='http://localhost:4005'
+export const baseUrl='http://49.232.0.86:5005'
 //备用2
-export const baseUrl2='http://localhost:5005'
+export const baseUrl2='http://localhost:4005'
 
 export default{
     baseUrl,baseUrl2
